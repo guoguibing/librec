3c1466dc67e3f8ca8b19cc8a4c7fc385ea38e667
Felix <Felix@Felix-PC>
2014-02-12 21:43:51 +0800

    NMF updated

--- librec/src/main/java/librec/ext/NMF.java
+++ librec/src/main/java/librec/ext/NMF.java
@@ -45,16 +45,7 @@ public class NMF extends IterativeRecommender {
 			loss = 0;
 			errs = 0;
 
-			// transpose of W
-			DenseMatrix trW = W.transpose();
-
-			// trW * V
-			DenseMatrix trW_V = trW.mult(V);
-
-			// trW * W * H
-			DenseMatrix trW_W_H = trW.mult(W).mult(H);
-
-			// transpose of H
+			// Step 1: update W by fixing H
 			DenseMatrix trH = H.transpose();
 
 			// V * trH
@@ -63,18 +54,27 @@ public class NMF extends IterativeRecommender {
 			// W * H * trH
 			DenseMatrix W_H_trH = W.mult(H.mult(trH));
 
-			// update: H_ij = H_ij * (trW_V)_ij / (trW_W_H)_ij
-			for (int i = 0; i < H.numRows(); i++)
-				for (int j = 0; j < H.numCols(); j++) {
-					double denorm = trW_W_H.get(i, j) + 1e-9;
-					H.set(i, j, H.get(i, j) * (trW_V.get(i, j) / denorm));
-				}
-
 			// update: W_ij = W_ij * (V_trH)_ij / (W_H_trH)_ij
 			for (int i = 0; i < W.numRows(); i++)
 				for (int j = 0; j < W.numCols(); j++) {
 					double denorm = W_H_trH.get(i, j) + 1e-9;
 					W.set(i, j, W.get(i, j) * (V_trH.get(i, j) / denorm));
+				}
+
+			// Step 2: update H by fixing W
+			DenseMatrix trW = W.transpose();
+
+			// trW * V
+			DenseMatrix trW_V = trW.mult(V);
+
+			// trW * W * H
+			DenseMatrix trW_W_H = trW.mult(W).mult(H);
+
+			// update: H_ij = H_ij * (trW_V)_ij / (trW_W_H)_ij
+			for (int i = 0; i < H.numRows(); i++)
+				for (int j = 0; j < H.numCols(); j++) {
+					double denorm = trW_W_H.get(i, j) + 1e-9;
+					H.set(i, j, H.get(i, j) * (trW_V.get(i, j) / denorm));
 				}
 
 			// compute errors
