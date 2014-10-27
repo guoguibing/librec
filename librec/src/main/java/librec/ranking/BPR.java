e38b3ec38eae69be00847ce7944aba22c464e1c8
guoguibing <guoguibing@gmail.com>
2014-10-27 11:52:38 +0800

    minor udpate

--- librec/src/main/java/librec/ranking/BPR.java
+++ librec/src/main/java/librec/ranking/BPR.java
@@ -63,7 +63,8 @@ public class BPR extends IterativeRecommender {
 		for (int iter = 1; iter <= numIters; iter++) {
 
 			if (verbose)
-				Logs.debug("Fold [{}] runs at iteration = {}", fold, iter);
+				Logs.debug("{}{} runs at iteration = {}", algoName, foldInfo,
+						iter);
 
 			for (int s = 0, smax = numUsers * 100; s < smax; s++) {
 
