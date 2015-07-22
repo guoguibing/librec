e6916fe96a65ab2b07f0a1420a42c3d170d97fb9
guoguibing <gbguo@smu.edu.sg>
2015-07-22 00:53:43 -0300

    description for tensor recommenders added

--- librec/src/main/java/librec/intf/TensorRecommender.java
+++ librec/src/main/java/librec/intf/TensorRecommender.java
@@ -1,11 +1,40 @@
+// Copyright (C) 2014-2015 Guibing Guo
+//
+// This file is part of LibRec.
+//
+// LibRec is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LibRec is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with LibRec. If not, see <http://www.gnu.org/licenses/>.
+//
+
 package librec.intf;
+
+import happy.coding.io.Strings;
 
 import java.util.List;
 
+import librec.data.Configuration;
 import librec.data.MatrixEntry;
 import librec.data.SparseMatrix;
 import librec.data.SparseTensor;
 
+/**
+ * Interface for tensor recommenders
+ * 
+ * @author Guo Guibing
+ *
+ */
+
+@Configuration("factors, lRate, maxLRate, reg, iters, boldDriver")
 public class TensorRecommender extends IterativeRecommender {
 
 	/* for all tensors */
@@ -20,7 +49,7 @@ public class TensorRecommender extends IterativeRecommender {
 		rateTensor = rateDao.getRateTensor();
 		numDimensions = rateTensor.numDimensions();
 		dimensions = rateTensor.dimensions();
-		
+
 		userDimension = rateTensor.getUserDimension();
 		itemDimension = rateTensor.getItemDimension();
 	}
@@ -33,19 +62,24 @@ public class TensorRecommender extends IterativeRecommender {
 		testTensor = new SparseTensor(dimensions);
 		testTensor.setUserDimension(userDimension);
 		testTensor.setItemDimension(itemDimension);
-		
+
 		for (MatrixEntry me : testMatrix) {
 			int u = me.row();
 			int i = me.column();
 
 			List<Integer> indices = rateTensor.getIndices(u, i);
 
-			for (int index: indices) {
+			for (int index : indices) {
 				int[] keys = rateTensor.keys(index);
 				testTensor.set(rateTensor.value(index), keys);
 				trainTensor.remove(keys);
 			}
 		}
+	}
+
+	@Override
+	public String toString() {
+		return Strings.toString(new Object[] { numFactors, initLRate, maxLRate, reg, numIters, isBoldDriver });
 	}
 
 }
