c8ef7c8834c2b7341c56d1f59687ff608d020b68
sunya <sunyatong65536@gmail.com>
2017-11-18 15:35:17 +0800

    fix FISMauc parameter updating bug

--- core/src/main/java/net/librec/recommender/cf/ranking/FISMaucRecommender.java
+++ core/src/main/java/net/librec/recommender/cf/ranking/FISMaucRecommender.java
@@ -153,7 +153,7 @@ public class FISMaucRecommender extends MatrixFactorizationRecommender {
                         DenseVector qi = Q.row(i).add(delta_qi.scale(lRate));
                         Q.setRow(i, qi);
                         DenseVector delta_qj = t.scale(e).minus(Q.row(j).scale(beta));
-                        DenseVector qj = Q.row(j).add(delta_qj.scale(lRate));
+                        DenseVector qj = Q.row(j).minus(delta_qj.scale(lRate));
                         Q.setRow(j, qj);
 
                         // update x
