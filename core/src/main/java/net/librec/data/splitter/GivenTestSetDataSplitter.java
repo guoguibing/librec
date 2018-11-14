eb7ed5e15ed745b40544ba32de5eedec100585fe
sunyatong <sunyatong@jd.com>
2018-11-14 16:04:32 +0800

    fix GivenTestSetDataSpliter

--- core/src/main/java/net/librec/data/splitter/GivenTestSetDataSplitter.java
+++ core/src/main/java/net/librec/data/splitter/GivenTestSetDataSplitter.java
@@ -21,7 +21,9 @@ import net.librec.common.LibrecException;
 import net.librec.conf.Configuration;
 import net.librec.conf.Configured;
 import net.librec.data.DataConvertor;
+import net.librec.data.convertor.ArffDataConvertor;
 import net.librec.data.convertor.TextDataConvertor;
+import net.librec.math.structure.MatrixEntry;
 import net.librec.math.structure.SequentialAccessSparseMatrix;
 
 import java.io.IOException;
@@ -62,28 +64,41 @@ public class GivenTestSetDataSplitter extends AbstractDataSplitter {
     public void splitData() throws LibrecException{
         DataConvertor testConvertor = null;
         String dataFormat = conf.get("data.model.format");
+        String[] inputDataPath = conf.get("data.testset.path").trim().split(":");
+        for (int i = 0; i < inputDataPath.length; i++) {
+            inputDataPath[i] = conf.get(Configured.CONF_DFS_DATA_DIR) + "/" + inputDataPath[i];
+        }
         switch (dataFormat.toLowerCase()){
             case "text":
-                String[] inputDataPath = conf.get("data.testset.path").trim().split(":");
-                for (int i = 0; i < inputDataPath.length; i++) {
-                    inputDataPath[i] = conf.get(Configured.CONF_DFS_DATA_DIR) + "/" + inputDataPath[i];
-                }
+
                 String dataColumnFormat = conf.get(Configured.CONF_DATA_COLUMN_FORMAT, "UIR");
                 testConvertor = new TextDataConvertor(dataColumnFormat,
                         inputDataPath,"[\t;, ]");
-                try {
-                    testConvertor.processData();
-                }catch (IOException e) {
-                    e.printStackTrace();
-                }
                 break;
+            case "arff":
+                testConvertor = new ArffDataConvertor(inputDataPath);
+                break;
+
             default:
                 LOG.info("Not implement now or please check data.model.format");
         }
 
+        try {
+            testConvertor.processData();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
 
         testMatrix = testConvertor.getPreferenceMatrix(conf);
         trainMatrix = dataConvertor.getPreferenceMatrix(conf);
+
+        // remove test elements from trainMatrix
+        for (MatrixEntry me : testMatrix) {
+            int rowIdx = me.row();
+            int colIdx = me.column();
+            trainMatrix.set(rowIdx, colIdx, 0.0);
+        }
+        trainMatrix.reshape();
 
 //        if (Objects.equals(conf.get("data.convert.columns"), null)){
 //            testMatrix = testConvertor.getPreferenceMatrix();
